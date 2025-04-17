fd7eb6032625bcdebb386c935aa4e4f14a6cab02
Arsenii Dezhin <dezhin.arsenii@lit1533.ru>
2025-04-17 20:50:48 +0300

    1

--- BackgroundTask17/Program.cs
+++ BackgroundTask17/Program.cs
@@ -2,44 +2,60 @@
 {
     private int base_1;
     private int base_2;
-    private int side;
+    private int h;
 
 
-    public Equilateral_Trapezoid(int base_1, int base_2, int side)
+    public Equilateral_Trapezoid(int base_1, int base_2, int h)
     {
         this.base_1 = base_1;
         this.base_2 = base_2;
-        this.side = side;
+        this.h = h;
     }
 
+    public Equilateral_Trapezoid()
+    {
+        this.base_1 = 5;
+        this.base_2 = 7;
+        this.h = 3;
+    }
+
+    private double Side()
+    {
+        double side = Math.Sqrt((this.h * this.h) + Math.Pow((this.base_1 -this.base_2)/2, 2));
+        return side;
+    }
     public void output()
     {
         Console.WriteLine($"основания: {base_1} и {base_2}");
-        Console.WriteLine($"боковые стороны - {side}");
+        Console.WriteLine($"высота - {h}");
+        Console.WriteLine($"стороны - {this.Side()}");
     }
 
     public void change(string side, int val)
     {
-         Console.WriteLine("Что хочешь поменять в своей трапеции? 1 - основание 1; 2 - основание 2; 3 - боковая сторона.");
+         Console.WriteLine("Что хочешь поменять в своей трапеции? 1 - основание 1; 2 - основание 2; 3 - высоту.");
+         side = Console.ReadLine();
+         Console.WriteLine("На сколько ты хочешь поменять?");
+         val = Convert.ToInt16(Console.ReadLine());
          switch (side)
          {
             case "1": this.base_1 = val; break;
             case "2": this.base_2 = val; break;
-            case "3": this.side = val; break;
+            case "3": this.h = val; break;
          }
     }
 
 
     public double Square()
     {
-        double S = ((this.base_1 + this.base_2)/2) * Math.Sqrt((this.side * this.side) - Math.Pow((this.base_1 - this.base_2)/2, 2));
+        double S = ((this.base_1 + this.base_2)/2) * this.h;
         return S;
     }
 
 
     public double Perimeter()
     {
-        double P = this.base_1 + this.base_2 + this.side * 2;
+        double P = this.base_1 + this.base_2 + this.Side() * 2;
         return P;
     }
 }
@@ -49,8 +65,8 @@ class Program
     static void Main()
     {
         int base_1, base_2, side;
-
-        Console.WriteLine("Ты хочешь создать р/б трапецию? y or n");
+        Equilateral_Trapezoid trap;
+        Console.WriteLine("Ты хочешь создать р/б трапецию сам? y or n");
         string ans = Console.ReadLine();
         if (ans == "y")
         {
@@ -61,23 +77,27 @@ class Program
             Console.WriteLine("Введите длину боковой стороны");
             side = Convert.ToInt16(Console.ReadLine());
 
-            Equilateral_Trapezoid trap_1 = new Equilateral_Trapezoid(base_1, base_2, side);
+            trap = new Equilateral_Trapezoid(base_1, base_2, side);
+        }
+        else
+        {
+            trap = new Equilateral_Trapezoid();
         }
 
         Console.WriteLine("Ты хочешь узнать периметр трапеции? y or n");
         ans = Console.ReadLine();
-        if (ans == "y");
+        if (ans == "y")
         {
-            Console.WriteLine("Периметр трапеции равен:");
-            Console.WriteLine(trap_1.Perimeter());
+            Console.Write("Периметр трапеции равен: ");
+            Console.WriteLine(trap.Perimeter());
         }
 
         Console.WriteLine("Ты хочешь узнать площадь трапеции? y or n");
         ans = Console.ReadLine();
-        if (ans = "y");
+        if (ans == "y")
         {
-            Console.WriteLine("Площадь трапеции равна:");
-            Console.WriteLine(trap_1.Square());
+            Console.Write("Площадь трапеции равна: ");
+            Console.WriteLine(trap.Square());
         }
     }
 }
