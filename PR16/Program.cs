2bc4361203408b56815a27f4e91b2818fd4e6fb1
Arsenii Dezhin <dezhin.arsenii@lit1533.ru>
2025-04-21 08:35:04 +0300

    1

--- PR16/Program.cs
+++ PR16/Program.cs
@@ -1,61 +1,151 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-class point3D
+﻿class Point3D
 {
-    public int x;
-    public int y;
-    public int z;
+    private double x;
+    private double y;
+    private double z;
 
-    public point3D()
+
+    public Point3D()
     {
-        x = 0;
-        y = 0;
-        z = 0;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
     }
 
-    public point3D(int a, int b, int c)
+
+    public Point3D(double x, double y, double z)
     {
-        x = a;
-        y = b;
-        z = c;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
-    public void print()
+
+    public Point3D(decimal n)
     {
-        Console.WriteLine($"x - {x}, y - {y}, z - {z}");
+        this.x = (double) n;
+        this.y = (double) n;
+        this.z = 0;
     }
 
-    public void change(string a, int b)
+
+    public void Move(string axis, double val)
     {
-        if (a == "x")
+        switch (axis)
         {
-            x += b;
-        }
-        else if (a == "y")
-        {
-            y += b;
-        }
-        else
-        {
-            z += b;
+            case "x": this.x = val; break;
+            case "y": this.y = val; break;
+            case "z": this.z = val; break;
         }
     }
 
 
-    public double radius_v()
+    public void Output()
     {
-        double R = Math.Sqrt(x * x + y * y + z * z);
-        return R;
+        Console.WriteLine($"x - {this.x}, y - {this.y}, z - {this.z}");
     }
 
 
-    public point3D plus(point3D otherPoint)
+    public double RVector()
     {
-        int newX = this.x + otherPoint.x;
-        int newY = this.y + otherPoint.y;
-        int newZ = this.z + otherPoint.z;
+        double r = Math.Sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+        return r;
+    }
 
-        return new point3D(newX, newY, newZ);
+
+    public Point3D Summ(Point3D point)
+    {
+        double x = this.x + point.x;
+        double y = this.y + point.y;
+        double z = this.z + point.z;
+        return new Point3D(x, y, z);
+    }
+
+
+    public Point3D Summ(double x, double y, double z)
+    {
+        double x_1 = x + this.x;
+        double y_1 = y + this.y;
+        double z_1 = z + this.z;
+        return new Point3D(x_1, y_1, z_1);
+    }
+
+
+    public Point3D Summ(double n)
+    {
+        double x = this.x + n;
+        double y = this.y + n;
+        double z = this.z + n;
+        return new Point3D(x, y, z);
+    }
+
+
+    public double X
+    {
+        get
+        {
+            return x;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                this.x = value;
+            }
+            else
+            {
+                Console.WriteLine("Значение не может быть отрицательным");
+            }
+        }
+    }
+
+
+    public double Y
+    {
+        get
+        {
+            return this.y;
+        }
+        set
+        {
+            if (value >= 0 && value <= 100)
+            {
+                this.y = value;
+            }
+            else
+            {
+                Console.WriteLine("Значение должно пренадлежать промежутку [0; 100]");
+            }
+        }
+    }
+
+
+    public double Z 
+    {
+        get
+        {
+            return this.z;
+        }
+        set
+        {
+            if (value <= this.x + this.y)
+            {
+                this.z = value;
+            }
+            else
+            {
+                Console.WriteLine("Z должна быть меньше чем X + Y");
+            }
+        }
+    }
+
+
+    public bool HasHit
+    {
+        get
+        {
+            
+        }
     }
 }
 
@@ -63,62 +153,50 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Ты хочешь ввести координаты сам? y or n");
-        string j = Console.ReadLine();
-        point3D point, point_1, point_2;
-
-        if (j == "n")
+        string ans, axis;
+        Point3D point1, point2, point3;
+        double x, y, z, val;
+        
+        Console.WriteLine("Ты хочешь создать точку сам? y/n");
+        ans = Console.ReadLine();
+        if (ans = "y")
         {
-            point = new point3D();
+            Console.WriteLine("Введи x своей точки");
+            x = Convert.ToInt16(Console.Readline());
+            Console.WriteLine("Введи y своей точки");
+            y = Convert.ToInt16(Console.Readline());
+            Console.WriteLine("Введи z своей точки");
+            z = Convert.ToInt16(Console.Readline());
+
+            point1 = new Point3D(x, y, z);
         }
         else
         {
-            int x = Convert.ToInt16(Console.ReadLine());
-            int y = Convert.ToInt16(Console.ReadLine());
-            int z = Convert.ToInt16(Console.ReadLine());
-            point = new point3D(x, y, z);
+            point1 = new Point3D();
+        }
+         
+        Console.WriteLine("Ты хочещь сдвинуть точку? y/n");
+        ans = Console.ReadLine();
+        if (ans = "y")
+        {
+            Console.WriteLine("По какой оси?");
+            axis = Console.ReadLine();
+            Console.WriteLine("На сколько");
+            val = Convert.ToInt16(Console.ReadLine());
+            
+            point1.Output();
+            point1.Move(axis, val);
+            point1.Output();
         }
 
-        /*Console.WriteLine("Какое количество команд ты хочешь выполнить?");
-        int s = Convert.ToInt16(Console.ReadLine());
-
-        for (int i = 0; i < s; i++)
-        {
-            Console.WriteLine("Измени одну из координат или вывести координаты твоей точки в консоль?");
-            string ans = Console.ReadLine();
-
-            if (ans == "print")
-            {
-                point.print();
-            }
-            else
-            {
-                Console.WriteLine("Координата которую хочешь изменить");
-                string h = Console.ReadLine();
-                Console.WriteLine("На сколько ты хочешь изменить её");
-                int cou = Convert.ToInt16(Console.ReadLine());
-                point.change(h, cou);
-            }
-        }*/
-
-        point_1 = new point3D();
-        point_1.change("X", 50);
-        point_1.change("y", 15);
         
-        Console.WriteLine("Радиус-вектор 1 точки:");
-        Console.WriteLine(point.radius_v());
-        Console.WriteLine("Радиус-вектор 2 точки:");
-        Console.WriteLine(point_1.radius_v());
-        
-        point_2 = new point3D();
-        point_2 = point.plus(point_1);
+        //PR16.2
+        point2 = new Point3D();
+        point2.Move("x", 50);
+        point2.Move("y", 15);
+        point2.Output();
+        Console.WriteLine($"Длина радиус-вектора 1 точки - {point1.RVector()}, длина радиус-вектора 2 точки - {point2.RVector()}");
+        point3 = point1.Summ(point2);
 
-        Console.WriteLine("результат сложения двух точек:");
-        point_2.print();
-
-        
-
-
-        Main();
     }
 }
