d741ac562e35309f52e493607c2b912118945dd1
Arsenii Dezhin <dezhin.arsenii@lit1533.ru>
2025-04-24 08:21:19 +0300

    Pr 17 is done

--- BackgroundTask6/obj/Debug/net9.0/BackgroundTask6.AssemblyInfo.cs
+++ BackgroundTask6/obj/Debug/net9.0/BackgroundTask6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackgroundTask6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc4361203408b56815a27f4e91b2818fd4e6fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1538029b512c1821cc5158e5d894680162ba60d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackgroundTask6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackgroundTask6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
